5660a5bee7248e3c00d21b0f59988ba8a8be7575
aplteam <kai@aplteam.com>
2020-05-05 20:22:00 +0100

    18.0 changes integrated

--- Make/Make.bat
+++ Make/Make.bat
@@ -1,3 +1,3 @@
 ; Note that the bridge and the DyalogNET DLL must be compatible to the version of APL called from here!
-"C:\Program Files (x86)\Dyalog\Dyalog APL 15.0 Unicode\dyalog.exe" %1 maxw=64000 %2 %3 %4
+"C:\Program Files (x86)\Dyalog\Dyalog APL 16.0 Unicode\dyalog.exe" %1 maxw=64000 %2 %3 %4
 IF %ERRORLEVEL% NEQ 0 echo ERROR
